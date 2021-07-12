440ec1b1c8833903fa6b579250428d7f120080d9
Roel <roelwinter11@gmail.com>
2021-07-12 13:55:45 +0200

    Add / path

--- myApp.js
+++ myApp.js
@@ -1,39 +1,10 @@
 var express = require('express');
 var app = express();
 
-console.log('Hello World')
+app.get('/', function(req, res) {
+    console.log(req);
+    console.log(res);
+    res.send('Response String');
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
