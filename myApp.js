36e0b7ebb58036a4350c09b795cc9bd5fcc4d6b9
Roel <roelwinter11@gmail.com>
2021-07-19 12:05:57 +0200

    Add logger

--- myApp.js
+++ myApp.js
@@ -8,7 +8,13 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html')
 })
 
-app.get('/json', function(req, res) {
+
+function logger (req, res, next) {
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next();
+}
+
+app.get('/json', logger, function(req, res) {
     let message = 'Hello json';
     if (process.env.MESSAGE_STYLE === 'uppercase') {
         message = message.toUpperCase();
